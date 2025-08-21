6f1edc00d6afceb5851c5c3e2c5cdae6fcecf44c
zhujianqin <jzhu0106@student.Monash.edu>
2025-08-22 02:37:12 +1000

    week3-studio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import JSON from './components/JSON.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+// import JSON from './components/JSON.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <Form />
+  <!-- <header> -->
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
       <JSON />
-    </div>
-  </header>
+    </div> -->
+  <!-- </header> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- <main> -->
+    <!-- <TheWelcome /> -->
+  <!-- </main> -->
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
 }
 
@@ -45,5 +47,5 @@ header {
     place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+} */
 </style>
